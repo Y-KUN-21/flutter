9bad71da0e85d127eea56bac83ece35020647e1b
Y-KUN-21 <57254049+Y-KUN-21@users.noreply.github.com>
2020-05-13 19:24:45 +0530

    Add files via upload

--- covidtracker2/lib/service/apiservice.dart
+++ covidtracker2/lib/service/apiservice.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
 import 'package:http/http.dart' as http;
 import 'package:gocorona/models/totals.dart';
 
@@ -6,13 +9,31 @@ class ApiServices {
   static Future<Data> getDataFromAPI() async {
     try {
       final response = await http.get(url);
-      if (200 == response.statusCode) {
-
-        final data = dataFromJson(response.body);
-
+      String filename = "cache.json";
+      var dir = await getTemporaryDirectory();
+      File file = new File(dir.path + "/$filename");
+      if (file.existsSync()) {
+        print("Loading from cache....");
+        var jsonData = file.readAsStringSync();
+        final data = dataFromJson(jsonData);
+          Timer.periodic(Duration(minutes: 120), (timer) {
+            try {
+              file.deleteSync(recursive: true);
+            } on FileSystemException {
+              print('File does\'nt exist.. wait');
+            }
+          });
         return data;
       } else {
-        return Data();
+        if (200 == response.statusCode) {
+          print("Loading from api....");
+          final data = dataFromJson(response.body);
+          file.writeAsStringSync(response.body,
+              flush: true, mode: FileMode.write);
+          return data;
+        } else {
+          return Data();
+        }
       }
     } catch (e) {
       print("Error" + (e));
