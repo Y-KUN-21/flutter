a2e7f98edd0dc57718b6bcb23f49f6e22ae23357
Y-KUN-21 <57254049+Y-KUN-21@users.noreply.github.com>
2020-05-13 19:23:47 +0530

    Add files via upload

--- covidtracker2/lib/widgets/preventions.dart
+++ covidtracker2/lib/widgets/preventions.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:gocorona/models/information.dart';
-
+import 'package:auto_size_text/auto_size_text.dart';
 class Prevention extends StatefulWidget {
   @override
   _PreventionState createState() => _PreventionState();
@@ -27,7 +27,7 @@ class _PreventionState extends State<Prevention> {
               itemCount: preventions.length,
               itemBuilder: ((context, index) {
                 return Container(
-                  height: size.height * .20,
+                  height: size.height * .25,
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.center,
                     children: <Widget>[
@@ -42,24 +42,36 @@ class _PreventionState extends State<Prevention> {
                             width: 150,
                             height: 120,
                           ),
-                          Expanded(
-                            child: RichText(
-                              text: TextSpan(
-                                text: preventions[index].title,
-                                style: TextStyle(
-                                    fontWeight: FontWeight.w400,
-                                    color: Colors.black,
-                                    fontSize: 30),
-                                children: <TextSpan>[
-                                  TextSpan(
-                                      text:
-                                          "\n${preventions[index].description}",
-                                      style: TextStyle(
-                                          fontWeight: FontWeight.w300,
-                                          fontSize: 20)),
+                           Expanded(
+                            child: Padding(
+                              padding: const EdgeInsets.only(left:8.0),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: <Widget>[
+                                  AutoSizeText(
+                                    preventions[index].title,
+                                    textAlign: TextAlign.left,
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.w400,
+                                      color: Colors.black,
+                                      fontSize: 25.0,
+                                    ),
+                                    maxFontSize: 20,
+                                    minFontSize: 15,
+                                    maxLines: 1,
+                                    
+                                  ),
+                                  AutoSizeText(
+                                    "\n${preventions[index].description}",
+                                    style: TextStyle(fontSize: 15,fontWeight: FontWeight.w300,fontFamily: "Poppins"),
+                                    maxFontSize: 15,
+                                    minFontSize: 5,
+                                    maxLines: 6,
+                                  )
                                 ],
                               ),
                             ),
+                            
                           ),
                         ],
                       ),
